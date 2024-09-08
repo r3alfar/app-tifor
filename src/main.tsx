f775257f1266276ee8643f5bdb9a09817fb6c354
r3alfar <farreladivia@gmail.com>
2024-09-08 10:53:29 +0700

    theme toggler

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from '@/views/app/Aapp.tsx'
 import './index.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+
   </StrictMode>,
 )
