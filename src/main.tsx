46b9e790acd75feb1b7fbf368049d41561a37b24
r3alfar <farreladivia@gmail.com>
2024-09-19 00:15:54 +0700

    off strict mode for development

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,15 +7,11 @@ import { Toaster } from './components/ui/toaster.tsx'
 import { AuthProvider } from './views/auth/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    {/* <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> */}
-    <AuthProvider>
-      <App />
-      <Toaster />
-      {/* <Auth /> */}
-    </AuthProvider>
 
-    {/* </ThemeProvider> */}
+  <AuthProvider>
+    <App />
+    <Toaster />
+  </AuthProvider>
 
-  </StrictMode>,
+
 )
